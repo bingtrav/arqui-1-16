7028171b07cc72a1ab30ce95abecaf30a854d338
andres25fg <andres25fgc@hotmail.com>
2016-04-28 18:41:13 +0000

    Diseno de estructuras

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,1 +1,16 @@
 /* Proyecto Arquitectura de Computadoras. */
+int main () {
+    // Cache de instrucciones que tiene cada CPU
+    int cache[4][17];
+    
+    //Memoria principal
+    int memPrin [384];
+    
+    //Registros de CPU
+    int reg[32];
+    
+    //Estructura de datos para los contextos
+    int contextos[4][33];
+    
+    return 0;
+}
