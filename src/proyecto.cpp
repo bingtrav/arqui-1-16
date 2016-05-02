e1aec08df11dc531c5b071a891728f27d38794c0
andres25fg <andres25fgc@hotmail.com>
2016-05-02 17:34:16 +0000

    cpp modificado

--- src/proyecto.cpp
+++ src/proyecto.cpp
@@ -1,17 +1,36 @@
 /* Proyecto Arquitectura de Computadoras. */
 
+// Se incluye la biclioteca de hilos
+#include <pthread.h>
+
+//PC del CPU
+int pc;
+    
+// Cache de instrucciones que tiene cada CPU
+int cache[4][17];
+    
+//Memoria principal
+int memPrin [384];
+    
+//Registros de CPU
+int reg[32];
+    
+//Estructura de datos para los contextos
+int contextos[4][33];
+
+int buscarBloque(int pc) {
+    int bloque = pc/16;
+    int palabra = pc%16;
+    int seccion = bloque%4;
+    
+    
+    if(cache[seccion][16] == bloque) {
+        
+    }
+}
+
 int main () {
-    // Cache de instrucciones que tiene cada CPU
-    int cache[4][17];
-    
-    //Memoria principal
-    int memPrin [384];
-    
-    //Registros de CPU
-    int reg[32];
-    
-    //Estructura de datos para los contextos
-    int contextos[4][33];
+
     
     return 0;
 }
