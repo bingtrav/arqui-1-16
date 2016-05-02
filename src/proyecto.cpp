5154fbff5465c302e4be2f43daec5b9a15ca68fd
andres25fg <andres25fgc@hotmail.com>
2016-05-02 18:25:38 +0000

    Fallo cache, buscar bloque y link util de pthreads

--- src/proyecto.cpp
+++ src/proyecto.cpp
@@ -1,10 +1,17 @@
 /* Proyecto Arquitectura de Computadoras. */
-
+// http://tuxthink.blogspot.com/2013/01/ussing-barriers-in-pthreads.html
 // Se incluye la biclioteca de hilos
 #include <pthread.h>
 
 //PC del CPU
 int pc;
+
+//Variable que contiene el quantum
+int quantum;
+
+// Barrera de control
+pthread_barrier_t barrera;
+//pthread_barrier_init(&barrera,NULL,3);
     
 // Cache de instrucciones que tiene cada CPU
 int cache[4][17];
@@ -23,17 +30,28 @@ int* buscarBloque(int pc) {
     int palabra = pc%16;
     int seccion = bloque%4;
     int plb[4];
+    int* plbPtr = plb;
     
-    if(cache[seccion][16] == bloque) {
-        for(int a = 0; a < 4; a++) {
-            plb[a] = cache[seccion][palabra];
-            palabra++;
-        }
-    } else {
-        
+    if(cache[seccion][16] != bloque) {
+        int dirFisica = bloque * 16;
+        falloCache(dirFisica, seccion);
     }
     
-    return plb;
+    for(int a = 0; a < 4; a++) {
+        plb[a] = cache[seccion][palabra];
+        palabra++;
+    }
+    
+    return plbPtr;
+}
+
+void falloCache (int direccion, int seccion) {
+    for(int a = 0; a < 16; a++) {
+        cache [seccion][a] = memPrin[direccion];
+        direccion++;
+        // Aqui va la barrera
+        //pthread_barrier_wait(&barrera);
+    }
 }
 
 int main () {
